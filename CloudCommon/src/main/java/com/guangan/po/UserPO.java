e517f8889a5d017aacf8af490360f7c7aab724d8
10203 <1020345729@qq.com>
2019-09-09 21:45:47 +0800

    新增common

---
+++ CloudCommon/src/main/java/com/guangan/po/UserPO.java
@@ -0,0 +1,32 @@
+package com.guangan.po;
+
+public class UserPO extends BasePO {
+    private String name;
+    private Integer age;
+    private Integer gender;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+}
